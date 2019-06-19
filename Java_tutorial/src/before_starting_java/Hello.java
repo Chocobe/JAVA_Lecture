635b4543a5e268c7a0a75930156704c657b3abef
Chocobe <nice_10bird@naver.com>
2019-06-19 16:24:05 +0900

    <master> : Created Hello.java

---
+++ Java_tutorial/src/before_starting_java/Hello.java
@@ -0,0 +1,23 @@
+package before_starting_java;
+
+/*
+ * 주석
+ */
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java!");
+		System.out.println("자바 응용 프로그램\n제작자 : 김영우");
+		System.out.println("번호\t이름\t전화번호\t");
+		System.out.println();
+		System.out.print("번호");
+		System.out.print("이름");
+		System.out.print("전화번호");
+		System.out.printf("%d%s", 1000, "korea\n");
+		
+		int num = 1;
+		int num2 = 100;
+		int res = num + num2;
+		System.out.println("res : " + res);
+	}
+}
---
+++ Java_tutorial/src/before_starting_java/README.md
@@ -0,0 +1,18 @@
+---
+---
+
+# before starting java 목차
+
+---
+---
+
+#### Hello.java
+		
+목적 : 문자열 출력하기 
+
+```java
+System.out.println("문자열");
+```
+
+---
+---
