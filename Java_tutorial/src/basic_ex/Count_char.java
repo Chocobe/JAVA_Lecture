4d9e84dfca91862e590eff7ce23b5bf223cfdc75
Chocobe <nice_10bird@naver.com>
2019-06-27 10:05:55 +0900

    <master> : Fixed Count_char

--- Java_tutorial/src/basic_ex/Count_char.java
+++ Java_tutorial/src/basic_ex/Count_char.java
@@ -8,19 +8,27 @@ public class Count_char {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		int consonant = 0;	// 자음 개수
-		int vowel = 0;		// 모음 개수
-		int digit = 0;		// 숫자 개수
+		int consonant_cnt = 0;	// 자음 개수
+		int vowel_cnt = 0;		// 모음 개수
+		int digit_cnt = 0;		// 숫자 개수
+		int others_cnt = 0;		// 기타 개수
 		
 		System.out.println("문제4] 문자열의 자음/모음 개수 구하기");
 
 		String input_data = "";
 		char single_string = ' ';
+		int ascii_val = 0;
 		int length = 0;
 		
 		String continuity = "";
 		
 		Main_loop : while(true) {
+			
+			consonant_cnt = 0;
+			vowel_cnt = 0;
+			digit_cnt = 0;
+			others_cnt = 0;
+			
 			System.out.println("-----------------------");
 			System.out.print("조사할 문자열을 입력하세요 : ");
 			input_data = scanner.nextLine();
@@ -33,33 +41,39 @@ public class Count_char {
 			
 			for(int i = 0; i < length; i++) {
 				single_string = input_data.charAt(i);
-
-//				switch(single_string) {
-//				case "a": case "e": case "i": case "o": case "u":
-//					vowel++;
-//					break;
-//				
-//				case "0": case "1": case "2": case "3": case "4":
-//				case "5": case "6": case "7": case "8": case "9":
-//					digit++;
-//					break;
-//					
-//				default :
-//					consonant++;
-//				}
+				ascii_val = (int)single_string;
 				
-				
-				
-				// <--------아스키코드값으로 검사
-				
-				
+				// 특수문자 검사
+				if((0 <= ascii_val && ascii_val <= 47) ||
+						(58 <= ascii_val && ascii_val <= 64) ||
+						(91 <= ascii_val && ascii_val <= 96) ||
+						(123 <= ascii_val && ascii_val <= 127)) {
+					others_cnt++;
+					
+				// 숫자 검사
+				} else if(48 <= ascii_val && ascii_val <= 57) {
+					digit_cnt++;
+					
+				// 모음 검사
+				} else if(ascii_val == 'A' || ascii_val == 'E' ||
+						  ascii_val == 'I' || ascii_val == 'O' ||
+						  ascii_val == 'U' || ascii_val == 'a' ||
+						  ascii_val == 'e' || ascii_val == 'i' ||
+						  ascii_val == 'o' || ascii_val == 'u') {
+					vowel_cnt++;
+					
+				// 자음 검사
+				} else {
+					consonant_cnt++;
+				}
 				
 			} // for(문자열 길이)
 			
 			System.out.println("입력문자 : " + input_data);
-			System.out.println("자음 개수 : " + consonant);
-			System.out.println("모음 개수 : " + vowel);
-			System.out.println("숫자 개수 : " + digit + "\n");
+			System.out.println("자음 개수 : " + consonant_cnt);
+			System.out.println("모음 개수 : " + vowel_cnt);
+			System.out.println("숫자 개수 : " + digit_cnt);
+			System.out.println("특수 문자 개수 : " + others_cnt + "\n");
 			
 			while(true) {
 				System.out.print("계속진행 하시겠습니까? (y/n) : ");
