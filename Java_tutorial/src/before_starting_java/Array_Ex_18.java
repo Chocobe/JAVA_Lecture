e304679efe370bb6a55658570d2d4dac083366d0
Chocobe <nice_10bird@naver.com>
2019-07-02 11:52:00 +0900

    <master> : Fixed README.md

--- Java_tutorial/src/before_starting_java/Array_Ex_18.java
+++ Java_tutorial/src/before_starting_java/Array_Ex_18.java
@@ -9,14 +9,14 @@ public class Array_Ex_18 {
 		
 		int sum = 0;
 		
-		// 2Â÷¿ø ¹è¿­À» Çâ»óµÈ for¹®À¸·Î »ç¿ë
+		// 2ì°¨ì› ë°°ì—´ì„ í–¥ìƒëœ forë¬¸ìœ¼ë¡œ ì‚¬ìš©
 		for(int[] i : score) {
 			for(int j : i) {
 				sum += j;
 			}			
 		}
 		
-		// 2Â÷¿ø ¹è¿­À» ÁßÃ¸ ¹Ýº¹¹®À¸·Î »ç¿ë
+		// 2ì°¨ì› ë°°ì—´ì„ ì¤‘ì²© ë°˜ë³µë¬¸ìœ¼ë¡œ ì‚¬ìš©
 		for(int i = 0; i < score.length; i++) {
 			for(int j = 0; j < score[i].length; j++) {
 				System.out.println("arr_num[" + i + "][" + j + "] : " + score[i][j]);
@@ -32,14 +32,31 @@ public class Array_Ex_18 {
 		int kor_sum = 0;
 		int eng_sum = 0;
 		int math_sum = 0;
+		int each_sum = 0;
+		float sub_avg = 0F;
 		
 		for(int i = 0; i < score.length; i++) {
+			
+			
 			kor_sum += score[i][0];
+			math_sum += score[i][1];
+			eng_sum += score[i][2];
+			
+			System.out.printf("%3d", i + 1);
+			
+			for(int j = 0; j < score[i].length; j++) {
+				each_sum += score[i][j];
+				System.out.printf("%5d", score[i][j]);
+			}
+			
+			sub_avg = (float)each_sum / score[i].length;
+			System.out.printf("%5d %5.1f\n", each_sum, sub_avg);
 		}
+		System.out.printf("ì´ì :%4d %4d %4d", kor_sum, math_sum, eng_sum);
 	}
 	
 	
-	// ¶óÀÎ Ãâ·Â
+	// ë¼ì¸ ì¶œë ¥
 	// @author	:	Chocobe
 	// @param	:	N/A
 	// @return	:	N/A
