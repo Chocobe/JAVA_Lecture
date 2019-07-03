17a3a2ecfa5ab1d55b5c6d4244491139b1ede2fb
Chocobe <nice_10bird@naver.com>
2019-07-03 09:33:38 +0900

    <master> : Fixed Class Block

--- Java_tutorial/src/nonogram/Block.java
+++ Java_tutorial/src/nonogram/Block.java
@@ -2,24 +2,29 @@ package nonogram;
 
 public class Block {
 	
-	// 출력부로 옮기자.
+	// 출력부로 옮기자. Class name : Render
 	public final static String WHITE  = "□";
 	public final static String BLACK  = "■";
 	public final static String V	  = "V";
 	public final static String X	  = "X";
 	
-	private Block_type block_is;
+	private Block_type guess_block;
 	private Block_type origin_block;
 	
 	
 	// 생성자
+	// @author	:	Chocobe
+	// @param	:	int _txt_data : .txt파일로 부터 읽어온 데이터
 	public Block(int _txt_data) {
-		this.block_is = Block_type.MUST_INIT;
+		this.guess_block = Block_type.MUST_INIT;
 		this.origin_block = convert_to_block(_txt_data);
 	}
 	
 	
-	// 원본 블록 설정
+	// 원본 블록 설정 (생성자 전용 메소드)
+	// @author	:	Chocobe
+	// @param	:	int _txt_data : .txt파일로 부터 읽어온 데이터
+	// @return	:	(Block_type) 블록값
 	private static Block_type convert_to_block(int _txt_data) {
 		if(_txt_data == 0) {
 			return Block_type.WHITE;
@@ -30,36 +35,45 @@ public class Block {
 	}
 	
 	
-	// 전체 블록 초기화
-	// create_block_array 로 이름변경 고려해볼 것 (변경시, 매개변수->row, col로 변경)
-	public static void init_all_block(Block[][] _block) {
+	// 전체 블록 레이지 초기화
+	// @author	:	Chocobe
+	// @param	:	Block[][] _block : 생성된 블록배열
+	// @return	:	N/A
+	public static Block[][] init_all_block(Block[][] _block) {
 		int row = _block.length;
 		int col = _block[0].length;
 		
+		Block[][] set_block = new Block[row][col];
+		
 		for(int i = 0; i < row; i++) {
 			for(int j = 0; j < col; j++) {
-				_block[i][j].block_is = Block_type.WHITE;
+				set_block[i][j].guess_block = Block_type.WHITE;
 			}
 		}
+		
+		return set_block;
 	}
 	
 	
-	// (답 입력) 원본 블록과 입력값의 비교 결과 반영
+	// (답 입력) 원본 블록과 추측 블록의 비교 결과 반영
+	// @author	:	Chocobe
+	// @param	:	Answer _key : 플레이어의 입력값 (O 또는 X)
+	// @return	:	N/A
 	public void send_answer(Answer _key) {
 		if(_key == Answer.O) {
 			if(this.origin_block == Block_type.BLACK) {
-				this.block_is = Block_type.BLACK;
+				this.guess_block = Block_type.BLACK;
 				
 			} else {
-				this.block_is = Block_type.X;
+				this.guess_block = Block_type.X;
 			}
 			
 		} else if(_key == Answer.X) {
-			if(this.block_is == Block_type.WHITE) {
-				this.block_is = Block_type.V;
+			if(this.guess_block== Block_type.WHITE) {
+				this.guess_block = Block_type.V;
 				
-			} else if(this.block_is == Block_type.V) {
-				this.block_is = Block_type.WHITE;
+			} else if(this.guess_block == Block_type.V) {
+				this.guess_block = Block_type.WHITE;
 			}
 		}
 	}
