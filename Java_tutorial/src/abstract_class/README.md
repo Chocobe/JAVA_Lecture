bc8058e7d3bb750810e0a1b31c6a6efb727c6671
Chocobe <nice_10bird@naver.com>
2019-07-08 12:22:36 +0900

    <master> : Created abstract_test_1

---
+++ Java_tutorial/src/abstract_class/README.md
@@ -0,0 +1,12 @@
+# abstract_class
+
+* [abstract_test_1](#abstract_test_1) - 추상 클래스 상속받기 예제_1
+
+---
+---
+
+### abstract_test_1
+
+추상 클래스 상속받기 예제_1
+
+:camel:[Top](#abstract_class)
---
+++ Java_tutorial/src/abstract_class/abstract_test_1/Claculator_test.java
@@ -0,0 +1,20 @@
+package abstract_class.abstract_test_1;
+
+public class Claculator_test {
+	public static void main(String[] args) {
+		Good_calc calculator = new Good_calc();
+		
+		int lhs = 10;
+		int rhs = 5;
+		
+		int[] val_arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		
+		int sum = calculator.add(lhs, rhs);
+		int sub = calculator.subtract(lhs, rhs);
+		double avg = calculator.average(val_arr);
+		
+		System.out.println(lhs + " + " + rhs + " = " + sum);
+		System.out.println(lhs + " - " + rhs + " = " + sub);
+		System.out.println("average of val_arr : " + avg);
+	}
+}
