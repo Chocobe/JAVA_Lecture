12745fc287391d9f9e5e83e84970948397bec125
Chocobe <nice_10bird@naver.com>
2019-07-09 10:28:56 +0900

    <master> : Created vector_1

---
+++ Java_tutorial/src/generic_collection/README.md
@@ -0,0 +1,12 @@
+# generic
+
+* [vector_1](#vector_1)
+
+---
+---
+
+### vector_1
+
+콜렉션 Vector 클래스 사용하기
+
+:camel:[Top](#generic)
---
+++ Java_tutorial/src/generic_collection/vectore_1/Vecter_ex.java
@@ -0,0 +1,81 @@
+package generic_collection.vectore_1;
+
+import java.util.Vector;
+
+public class Vecter_ex {
+	public static void main(String[] args) {
+		Vector<Integer> vector = new Vector<Integer>();
+		vector.add(5);
+		vector.add(100);
+		vector.add(50);
+		vector.add(2, 500);
+		vector.add(5);
+		vector.add(100);
+		vector.add(50);
+		vector.add(2, 500);
+		vector.add(5);
+		vector.add(100);
+		vector.add(50);
+		vector.add(2, 500);
+		
+		System.out.println("벡터 내의 개수 : " + vector.size());
+		System.out.println("벡터 현재 용량 : " + vector.capacity());
+		
+		for(int i = 0; i < vector.size(); i++) {
+			int n = vector.get(i);
+			System.out.printf("%5d", n);
+		}
+		System.out.println();
+		
+		int sum = 0;
+		for(int i = 0; i < vector.size(); i++) {
+			int n = vector.get(i);
+			sum += n;
+		}
+		System.out.println("총 합 : " + sum);
+
+		for(int i = 0; i < vector.size(); i++) {
+			int n = vector.elementAt(i);
+			System.out.printf("%5d", n);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
