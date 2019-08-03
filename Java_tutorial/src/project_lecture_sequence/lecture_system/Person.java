a2e6555144b512380194fafb73470d017ac5079f
Chocobe <nice_10bird@naver.com>
2019-08-03 16:44:22 +0900

    <master> : Created interface Person

---
+++ Java_tutorial/src/project_lecture_sequence/lecture_system/Person.java
@@ -0,0 +1,8 @@
+package project_lecture_sequence.lecture_system;
+
+public interface Person {
+	public abstract void go_to_work();
+	public abstract void go_home();
+	public abstract void have_lunch();
+	public abstract void rest();
+}
