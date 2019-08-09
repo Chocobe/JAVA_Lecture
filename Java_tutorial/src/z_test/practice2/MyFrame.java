c69688e03ee9fde4bc35928706c680be9522b8a0
Chocobe <nice_10bird@naver.com>
2019-08-09 13:47:59 +0900

    <master> : Created practice2

--- Java_tutorial/src/z_test/practice2/MyFrame.java
+++ Java_tutorial/src/z_test/practice2/MyFrame.java
@@ -1,29 +1,63 @@
 package z_test.practice2;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class MyFrame extends JFrame {
-	private JPanel centerPanel;
-	private JPanel southPanel;
+	private JPanel panel_1;
+	private JTable table;
+	private DefaultTableModel model;
+	private JScrollPane scroll;
+	
 	
 	public MyFrame() {
-		super();
-		initFrame();
+		this.setBounds(500, 300, 300, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		initTable();
+		initPanel();
+		combineComponents();
 	}
 	
 	
-	private void initFrame() {
-		this.setBounds(700, 400, 500, 300);
-		this.setTitle("테이블 테스트");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private void initTable() {
+		model = new DefaultTableModel();
+		model.addColumn("이름");
+		model.addColumn("번호");
+		
+		table = new JTable(model);
+		table.getTableHeader().setReorderingAllowed(false);
+		table.getTableHeader().setResizingAllowed(false);
+		table.setEnabled(false);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		
+		scroll = new JScrollPane();
+		scroll.setViewportView(table);
 	}
 	
 	
 	private void initPanel() {
-		this.centerPanel = new CenterPanel();
+		TitledBorder border = BorderFactory.createTitledBorder(new LineBorder(Color.BLACK), "타이틀");
 		
+		panel_1 = new JPanel(new BorderLayout());
+		panel_1.setBorder(border);		
 	}
+	
+	
+	private void combineComponents() {
+		panel_1.add(scroll, BorderLayout.CENTER);
+		this.getContentPane().add(panel_1);
+	}
+	
 	
 	public void runFrame() {
 		this.setVisible(true);
