3f34dc1620e7b51bd4a4aad1e401b58afa9125ea
Chocobe <nice_10bird@naver.com>
2019-08-10 00:51:51 +0900

    <master> : Fixed class WeaponDTO_unitTest

--- Java_tutorial/src/project_07_MonsterHunter_Weapon_DB/WeaponDTO/WeaponDTO.java
+++ Java_tutorial/src/project_07_MonsterHunter_Weapon_DB/WeaponDTO/WeaponDTO.java
@@ -1,5 +1,137 @@
 package project_07_MonsterHunter_Weapon_DB.WeaponDTO;
 
 public class WeaponDTO {
-	
+	private String name;
+	private String sort;
+	private int damage;
+	private int critical;
+	private int grade;
+	private String attribute;
+	private int attributeVal;
+	private int slot_1;
+	private int slot_2;
+	private int slot_3;
+	private String material;
+	private String tree;
+	private int curTreeNo;
+	private int sharpness_1;
+	private int sharpness_2;
+	private int sharpness_3;
+	private int sharpness_4;
+	private int sharpness_5;
+	private int sharpness_6;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSort() {
+		return sort;
+	}
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	public int getCritical() {
+		return critical;
+	}
+	public void setCritical(int critical) {
+		this.critical = critical;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getAttribute() {
+		return attribute;
+	}
+	public void setAttribute(String attribute) {
+		this.attribute = attribute;
+	}
+	public int getAttributeVal() {
+		return attributeVal;
+	}
+	public void setAttributeVal(int attributVal) {
+		this.attributeVal = attributVal;
+	}
+	public int getSlot_1() {
+		return slot_1;
+	}
+	public void setSlot_1(int slot_1) {
+		this.slot_1 = slot_1;
+	}
+	public int getSlot_2() {
+		return slot_2;
+	}
+	public void setSlot_2(int slot_2) {
+		this.slot_2 = slot_2;
+	}
+	public int getSlot_3() {
+		return slot_3;
+	}
+	public void setSlot_3(int slot_3) {
+		this.slot_3 = slot_3;
+	}
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	public String getTree() {
+		return tree;
+	}
+	public void setTree(String tree) {
+		this.tree = tree;
+	}
+	public int getCurTreeNo() {
+		return curTreeNo;
+	}
+	public void setCurTreeNo(int curTreeNo) {
+		this.curTreeNo = curTreeNo;
+	}
+	public int getSharpness_1() {
+		return sharpness_1;
+	}
+	public void setSharpness_1(int sharpness_1) {
+		this.sharpness_1 = sharpness_1;
+	}
+	public int getSharpness_2() {
+		return sharpness_2;
+	}
+	public void setSharpness_2(int sharpness_2) {
+		this.sharpness_2 = sharpness_2;
+	}
+	public int getSharpness_3() {
+		return sharpness_3;
+	}
+	public void setSharpness_3(int sharpness_3) {
+		this.sharpness_3 = sharpness_3;
+	}
+	public int getSharpness_4() {
+		return sharpness_4;
+	}
+	public void setSharpness_4(int sharpness_4) {
+		this.sharpness_4 = sharpness_4;
+	}
+	public int getSharpness_5() {
+		return sharpness_5;
+	}
+	public void setSharpness_5(int sharpness_5) {
+		this.sharpness_5 = sharpness_5;
+	}
+	public int getSharpness_6() {
+		return sharpness_6;
+	}
+	public void setSharpness_6(int sharpness_6) {
+		this.sharpness_6 = sharpness_6;
+	}
 }
