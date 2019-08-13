f661b04bba633a8e7d40256f3fa09c3861a2aab2
Chocobe <nice_10bird@naver.com>
2019-08-13 12:01:39 +0900

    <master> : Created class AttributePanel

---
+++ Java_tutorial/src/project_07_MonsterHunter_Weapon_DB/weapons_DB_gui/optionPanels/AttributePanel.java
@@ -0,0 +1,100 @@
+package project_07_MonsterHunter_Weapon_DB.weapons_DB_gui.optionPanels;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+
+import project_07_MonsterHunter_Weapon_DB.weapons_DB_gui.MainFrame;
+
+@SuppressWarnings("serial")
+public class AttributePanel extends OptionPanel {
+	private String selectedOption;
+	
+	public AttributePanel(MainFrame frame) {
+		super.initItsPanel(frame, "속성", 80);
+		initPanel();
+	}
+	
+	public void initPanel() {
+		ActionListener listener = new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				selectedOption = e.getActionCommand();
+				System.out.println("Attribute Category : " + selectedOption);
+			}
+		};
+		
+	// 그룹
+		ButtonGroup group = new ButtonGroup();
+		
+	// 버튼 생성
+		JRadioButton all = new JRadioButton("전체");
+		all.setBackground(Color.WHITE);
+		all.addActionListener(listener);
+		group.add(all);
+		this.add(all);
+		
+		JRadioButton fire = new JRadioButton("불속성");
+		fire.setBackground(Color.WHITE);
+		fire.addActionListener(listener);
+		group.add(fire);
+		this.add(fire);
+		
+		JRadioButton water = new JRadioButton("물속성");
+		water.setBackground(Color.WHITE);
+		water.addActionListener(listener);
+		group.add(water);
+		this.add(water);
+		
+		JRadioButton thunder = new JRadioButton("뇌속성");
+		thunder.setBackground(Color.WHITE);
+		thunder.addActionListener(listener);
+		group.add(thunder);
+		this.add(thunder);
+		
+		JRadioButton ice = new JRadioButton("빙속성");
+		ice.setBackground(Color.WHITE);
+		ice.addActionListener(listener);
+		group.add(ice);
+		this.add(ice);
+		
+		JRadioButton dragon = new JRadioButton("용속성");
+		dragon.setBackground(Color.WHITE);
+		dragon.addActionListener(listener);
+		group.add(dragon);
+		this.add(dragon);
+		
+		JRadioButton paralysis = new JRadioButton("마비속성");
+		paralysis.setBackground(Color.WHITE);
+		paralysis.addActionListener(listener);
+		group.add(paralysis);
+		this.add(paralysis);
+		
+		JRadioButton sleep = new JRadioButton("수면속성");
+		sleep.setBackground(Color.WHITE);
+		sleep.addActionListener(listener);
+		group.add(sleep);
+		this.add(sleep);
+		
+		JRadioButton poison = new JRadioButton("독속성");
+		poison.setBackground(Color.WHITE);
+		poison.addActionListener(listener);
+		group.add(poison);
+		this.add(poison);
+		
+		JRadioButton explosion = new JRadioButton("폭파속성");
+		explosion.setBackground(Color.WHITE);
+		explosion.addActionListener(listener);
+		group.add(explosion);
+		this.add(explosion);
+	}
+	
+	public String getSelectedOption() {
+		return selectedOption;
+	}
+}
